d8dbad2348cfcd8d1f1e6ae342295ea8e39a6eca
Gamerator190 <gamerator190@gmail.com>
2025-11-23 09:46:34 +0800

    Add seating page, add auditorium layout image

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Seating } from './pages/seating/seating';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    component: Home,
+  },
+  {
+    path: 'seating',
+    component: Seating
+  }
+//   {
+//     path: 'event',
+//     component: Event,
+//   },
+//   {
+//     path: 'event/:id',
+//     component: Detail,
+//   },
+];
---
+++ src/app/pages/seating/seating.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-seating',
+  imports: [],
+  templateUrl: './seating.html',
+  styleUrl: './seating.css',
+})
+export class Seating {
+
+}
