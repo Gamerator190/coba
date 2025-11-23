679049f12c8c8dfc4a0a25635df61ff9ea1484c2
Gamerator190 <gamerator190@gmail.com>
2025-11-23 13:13:06 +0800

    Trying to add seating

--- src/app/pages/seating/seating.ts
+++ src/app/pages/seating/seating.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, Inject, NgZone } from '@angular/core';
+import { DOCUMENT } from '@angular/core';
 
 @Component({
   selector: 'app-seating',
@@ -7,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './seating.css',
 })
 export class Seating {
+  seats = [
+    {id: 'A1', x:930, y:130, status: 'available'},
+  ]
+  constructor(@Inject(DOCUMENT) private document: Document, private zone: NgZone) {}
+  ngAfterViewInit(): void {
+    this.zone.runOutsideAngular(() => {
+      this.seats.forEach(seat => {
+        const svg = this.document.querySelector<SVGSVGElement>('#seat-layer')!;
 
+      const circle = this.document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+      circle.setAttribute('cx', seat.x.toString());
+      circle.setAttribute('cy', seat.y.toString());
+      circle.setAttribute('r', '8');
+      // circle.setAttribute('class', 'seat');
+      circle.setAttribute('fill', 'blue');
+
+      circle.addEventListener('click', () => {
+        alert('Clicked test seat!');
+      });
+
+      svg.appendChild(circle);
+      })
+      
+    })
+    
+  }
 }
