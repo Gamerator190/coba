2a9be9cf35f21d2f82fc00cfec339c81ec0894f9
Gamerator190 <gamerator190@gmail.com>
2025-11-27 10:44:18 +0800

    Fixed unavailable seats value in event page

--- src/app/services/event.ts
+++ src/app/services/event.ts
@@ -4,37 +4,43 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class Event {
-   data = [
-    {
-      id: 1,
-      picture: 'https://picsum.photos/id/10/2500/1667',
-      title: 'Christmas celebration',
-      time: 'Time: 12:00 AM - 02:00 PM',
-      date: 'Date: 25th December 2025',
-      location: 'Location: Central Park, New York City',
-      description: 'Join us for a festive celebration in Central Park with live music, food stalls, and holiday activities for all ages. Don\'t miss the tree lighting ceremony at dusk!',
-      ticketAvailability: 617,
-      promoCode: 'WELCOME15',
-      promoValue: 15,
-      unavailableSeats: ['a-1', 'b-2', 'c-3', 'd-4', 'e-5']
-    },
-    // {
-    //   id: 2,
-    //   picture: 'https://picsum.photos/id/11/200/100',
-    //   title: 'Title 2',
-    //   description: 'Description 2',
-    // },
-    // {
-    //   id: 3,
-    //   picture: 'https://picsum.photos/id/11/200/100',
-    //   title: 'Title 3',
-    //   description: 'Description 3',
-    // },
-    // {
-    //   id: 4,
-    //   picture: 'https://picsum.photos/id/11/200/100',
-    //   title: 'Title 4',
-    //   description: 'Description 4',
-    // },
-  ];
+  private unavailableSeatsEvent1 = ['a-1', 'b-2', 'c-3', 'd-4', 'e-5', 'f-6']; //temporary
+  
+  data: any[];
+
+  constructor() {
+    this.data = [
+      {
+        id: 1,
+        picture: 'https://picsum.photos/id/10/2500/1667',
+        title: 'Christmas celebration',
+        time: 'Time: 12:00 AM - 02:00 PM',
+        date: 'Date: 25th December 2025',
+        location: 'Location: Central Park, New York City',
+        description: 'Join us for a festive celebration in Central Park with live music, food stalls, and holiday activities for all ages. Don\'t miss the tree lighting ceremony at dusk!',
+        ticketAvailability: 617 - this.unavailableSeatsEvent1.length,
+        promoCode: 'WELCOME15',
+        promoValue: 15,
+        unavailableSeats: this.unavailableSeatsEvent1
+      },
+      // {
+      //   id: 2,
+      //   picture: 'https://picsum.photos/id/11/200/100',
+      //   title: 'Title 2',
+      //   description: 'Description 2',
+      // },
+      // {
+      //   id: 3,
+      //   picture: 'https://picsum.photos/id/11/200/100',
+      //   title: 'Title 3',
+      //   description: 'Description 3',
+      // },
+      // {
+      //   id: 4,
+      //   picture: 'https://picsum.photos/id/11/200/100',
+      //   title: 'Title 4',
+      //   description: 'Description 4',
+      // },
+    ];
+  }
 }
